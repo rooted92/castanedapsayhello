0b678d7e122d0c1406cb23821f6e197032c93a80
Pedro Castaneda <petecast76@gmail.com>
2022-10-17 14:13:31 -0700

    first push say hello

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿//Pedro Castaneda
+//10-17-2022
+//Say Hello
+//Simple program that asks your name and gives a response
+
+string username;
+
+Console.WriteLine("Hello, what is your name?");
+
+username = Console.ReadLine();
+
+Console.WriteLine($"Nice to meet {username}");
