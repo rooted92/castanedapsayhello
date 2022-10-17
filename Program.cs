bdfe0e8245990970902f695b42e0c00831caf7dd
Pedro Castaneda <petecast76@gmail.com>
2022-10-17 14:28:40 -0700

    Added validation and while loop features

--- Program.cs
+++ Program.cs
@@ -4,9 +4,32 @@
 //Simple program that asks your name and gives a response
 
 string username;
+string userInput;
+bool run = false;
 
-Console.WriteLine("Hello, what is your name?");
+while(!run)
+{
+    Console.WriteLine("Hello, what is your name?");
 
-username = Console.ReadLine();
+    username = Console.ReadLine();
 
-Console.WriteLine($"Nice to meet {username}");
+    Console.WriteLine($"Nice to meet {username}");
+    Console.WriteLine();
+
+    Console.WriteLine("Would you like run this program again? (yes/no)");
+    userInput = Console.ReadLine();
+
+    if(userInput == "yes" || userInput == "y")
+    {
+        run = false;
+    }
+    else if(userInput == "no" || userInput == "n")
+    {
+        Console.WriteLine($"Goodbye {username}!");
+        run = true;
+    }
+    else
+    {
+        Console.WriteLine("Please enter yes or no!");
+    }
+}
